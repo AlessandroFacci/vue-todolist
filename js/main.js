1fa61563a214dcb13fa85d50bab985d58924fd15
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-08-31 16:03:20 +0200

    js: add  to do list array

--- js/main.js
+++ js/main.js
@@ -3,7 +3,12 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      message: "Hello Vue!",
+      toDoList: [
+        {
+          text: "",
+          check: false,
+        },
+      ],
     };
   },
 }).mount("#app");
