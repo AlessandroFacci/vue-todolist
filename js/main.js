f826340065cc72d440fd02dc8dbef37074fa737e
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-09-01 11:29:51 +0200

    all: edit

--- js/main.js
+++ js/main.js
@@ -3,12 +3,37 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
+      title: "To Do List",
+
       toDoList: [
         {
-          text: "",
-          check: false,
+          text: "Fare la spesa",
+          done: true,
+        },
+        {
+          text: "Chiamare il dentista",
+          done: false,
+        },
+        {
+          text: "Pulire casa",
+          done: false,
         },
       ],
+
+      newTask: {
+        text: "",
+        done: false,
+      },
     };
+  },
+  methods: {
+    deleteTask(index) {
+      this.toDoList.splice(index, 1);
+    },
+    addNewTask() {
+      const newTaskCopy = { ...this.newTask };
+
+      this.toDoList.push(newTaskCopy);
+    },
   },
 }).mount("#app");
